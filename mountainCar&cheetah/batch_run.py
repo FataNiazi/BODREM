8f83dab6746b09e8a7527f97d0b88ab2f607d2e5
Kevaan Buch <kevaan.buch@mail.utoronto.ca>
2026-04-08 03:10:48 -0400

    removing legacy

--- mountainCar&cheetah/batch_run.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from .runner import run_batch
-
-__all__ = ["run_batch"]
--- mountainCar&cheetah/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .io import ensure_run_dir, slugify, timestamp, write_csv_rows, write_json
-
-__all__ = ["timestamp", "slugify", "ensure_run_dir", "write_json", "write_csv_rows"]
